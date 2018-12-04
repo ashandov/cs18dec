947cccbdf801576fc7e17786f832f15e7aa5d96a
a.shandov <anatolyshandov@gmail.com>
2018-12-04 17:23:16 +0300

    version 2 of lesson 1 (tasks:1,2,3,6,7)

---
+++ Lab1/Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 4131");
+            string asdf;
+            string qaz;
+            asdf = "фыва";
+            qaz = asdf;
+            Console.WriteLine(asdf);
+            Console.WriteLine(qaz);
+            
+
+        }
+    }
+}
---
+++ Lab1/Task03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задача 7365");
+            string hjkl, wsx;
+            hjkl = "ролд";
+            wsx = hjkl + "цычувс";
+            Console.WriteLine(hjkl);
+            Console.WriteLine(wsx);
+
+        }
+    }
+}
